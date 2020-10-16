3b4476c789e5b3dafe5bb0a1f2097ba6dc60d71d
Dominique Vilain <dominique.vilain@hepl.be>
2020-10-16 13:02:28 +0200

    add web routes

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\MatchController;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\TotoController;
+use App\Models\Match;
+use App\Models\Team;
+use App\Models\Tournament;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/s', function () {
+    return Team::first();
+});
+
+Route::get('/', [DashboardController::class, 'index']);
+
+Route::get('/match/create', [MatchController::class, 'create'])
+    ->name('new_match')
+    ->middleware(['auth', 'can:create,'.Match::class]);
+
+Route::post('/match', [MatchController::class, 'store'])
+    ->name('store_match')
+    ->middleware(['auth', 'can:create,'.Match::class]);
+
+Route::get('team/create',
+    [TeamController::class, 'create'])
+    ->name('new_team')
+    ->middleware(['auth', 'can:create,'.Team::class]);
+
+Route::get('team/{team:slug}/edit',
+    [TeamController::class, 'edit'])
+    ->name('change_team')
+    ->middleware(['auth', 'can:create,'.Team::class]);
+
+Route::post('team', [TeamController::class, 'store'])
+    ->name('store_team')
+    ->middleware(['auth', 'can:create,'.Team::class]);
+
+Route::patch('team/{team}', [TeamController::class, 'update'])
+    ->name('update_team')
+    ->middleware(['auth', 'can:create,'.Team::class]);
