f627560f793406e73fca5daceb90d60b18a4c63b
Dominique Vilain <dominique.vilain@hepl.be>
2020-10-19 20:01:02 +0200

    update Tournament Seeder to fetch logos

--- database/seeders/TournamentSeeder.php
+++ database/seeders/TournamentSeeder.php
@@ -15,25 +15,38 @@ class TournamentSeeder extends Seeder
      */
     public function run()
     {
-        Tournament::create([
+        $t = Tournament::create([
             'competition_id' => Competition::whereName('Premier League')->first()->id,
             'span_years' => '2020-2021',
             'file_name' => ''
         ]);
-        Tournament::create([
+        $t->addMediaFromUrl('https://upload.wikimedia.org/wikipedia/fr/f/f2/Premier_League_Logo.svg')
+            ->toMediaCollection('tournaments');
+
+        $t = Tournament::create([
             'competition_id' => Competition::whereName('Serie A')->first()->id,
             'span_years' => '2020-2021',
             'file_name' => ''
         ]);
-        Tournament::create([
+        $t->addMediaFromUrl('https://upload.wikimedia.org/wikipedia/commons/b/b4/Serie_A_Logo_%28ab_2019%29.svg')
+            ->toMediaCollection('tournaments');
+
+
+        $t = Tournament::create([
             'competition_id' => Competition::whereName('La Liga')->first()->id,
             'span_years' => '2020-2021',
             'file_name' => ''
         ]);
-        Tournament::create([
+        $t->addMediaFromUrl('https://upload.wikimedia.org/wikipedia/commons/9/92/LaLiga_Santander.svg')
+            ->toMediaCollection('tournaments');
+
+        $t = Tournament::create([
             'competition_id' => Competition::whereName('FA Cup')->first()->id,
             'span_years' => '2020-2021',
             'file_name' => ''
         ]);
+        $t->addMediaFromUrl('https://upload.wikimedia.org/wikipedia/fr/6/68/FA_Cup_logo.svg')
+            ->toMediaCollection('tournaments');
+
     }
 }
