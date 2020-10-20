f4daa3b4434bfcf935d406f3e4451ceadfbff136
Dominique Vilain <dominique.vilain@hepl.be>
2020-10-20 17:09:01 +0200

    add livewire support to app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
@@ -18,6 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @livewireStyles
 </head>
 <body>
 <div id="app">
@@ -79,5 +81,6 @@
         @yield('content')
     </main>
 </div>
+@livewireScripts
 </body>
 </html>
